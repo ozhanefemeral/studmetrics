f75ddbcc30894af6e2b00fe555f0b937b1408d2e
Özhan Efe Meral <ozhanefemeral@gmail.com>
2020-03-02 11:13:02 +0300

    deneme

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use('/api/assignments', assignmentsRoute)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/public/'));
+    // app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 app.listen(port, async () => {
