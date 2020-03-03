e6dad7943b3bd6d0f741918d75b27f32d8318d13
Özhan Efe Meral <ozhanefemeral@gmail.com>
2020-03-03 23:18:48 +0300

    added vue router express history fallback

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')
+const history = require('connect-history-api-fallback')
 const app = express()
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
-
 const db = require('./models/index')
 db.sequelize.sync()
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(history());
 
 const schoolRoute = require('./routes/schools')
 const teacherRoute = require('./routes/teachers')
