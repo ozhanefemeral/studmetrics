fd7afac174eb66302ecfdfaa76a760853e007b90
Özhan Efe Meral <ozhanefemeral@gmail.com>
2020-03-08 02:18:28 +0300

    builded client and added homework grading

--- routes/assignments.js
+++ routes/assignments.js
@@ -20,19 +20,33 @@ router.get('/:assignmentId', auth, async (req, res) => {
 })
 
 router.patch('/', auth, async (req, res) => {
-    Assignment.findOne({
+    req.body.mark = 0;
+
+    const assignment = await Assignment.findOne({
         where: {
             studentId: req.query.studentId,
             homeworkId: req.query.homeworkId
         }
-    }).then(assignment => {
-        return assignment.update(req.body)
-    }).then(assignment => {
-        res.send(assignment)
-    }).catch(err => {
-        console.log(err);
-        res.status(400).send()
     })
+
+    const homework = await assignment.getHomework()
+    console.log(homework.dataValues);
+
+
+    for (let i = 0; i < homework.dataValues.questions.length; i++) {
+        console.log(i);
+
+        const question = homework.dataValues.questions[i];
+        if (question.answer != undefined) {
+            if (assignment.answers[i] == question.answer) {
+                req.body.mark += question.point
+            }
+        }
+    }
+
+    await assignment.update(req.body)
+
+    res.send(assignment)
 })
 
 module.exports = router;
